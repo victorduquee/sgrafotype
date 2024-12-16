f665575ee170154e3d6b466e8e1eba42b8ad489c
victorduquee <victorduquedesign@gmail.com>
2024-12-16 01:19:01 +0100

    SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,25 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "nuxt-aos", 'nuxt-anchorscroll'],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-aos", "nuxt-anchorscroll"],
   anchorscroll: {
     hooks: [
       // Or any valid hook if needed
       // Default is `page:finish`
-      'page:transition:finish',
+      "page:transition:finish",
     ],
   },
   css: ["~/assets/css/main.css"],
   app: {
     head: {
+      title: "SGrafoType",
+      meta: [
+        {
+          name: "description",
+          content: "SGrafoType, una familia tipográfica creada específicamente para el proyecto, es el corazón de un proyecto que busca dotar a Segovia de una identidad gráfica única. Un enfoque que respeta su legado, refleja su espíritu acogedor y permite comunicar su esencia de manera coherente y auténtica al mundo.",
+        },
+        { name: "keywords", content: "esgrafiado, tipografia, segovia" },
+      ],
       link: [
         {
           rel: "stylesheet",
@@ -21,16 +29,16 @@ export default defineNuxtConfig({
       ],
     },
     pageTransition: {
-      name: 'page',
-      mode: 'out-in' // default
+      name: "page",
+      mode: "out-in", // default
     },
   },
   aos: {
     // Global settings:
     disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-    initClassName: 'aos-init', // class applied after initialization
-    animatedClassName: 'aos-animate', // class applied on animation
+    startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: "aos-init", // class applied after initialization
+    animatedClassName: "aos-animate", // class applied on animation
     useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
     disableMutationObserver: false, // disables automatic mutations' detections (advanced)
     debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
@@ -40,9 +48,9 @@ export default defineNuxtConfig({
     offset: 120, // offset (in px) from the original trigger point
     delay: 0, // values from 0 to 3000, with step 50ms
     duration: 400, // values from 0 to 3000, with step 50ms
-    easing: 'ease', // default easing for AOS animations
+    easing: "ease", // default easing for AOS animations
     once: false, // whether animation should happen only once - while scrolling down
     mirror: false, // whether elements should animate out while scrolling past them
-    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-  }
+    anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+  },
 });
