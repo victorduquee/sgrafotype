598a52e5a79385bf107d333e75496bdf908ba064
victorduquee <victorduquedesign@gmail.com>
2025-01-06 21:17:27 +0100

    update

--- pages/aplicaciones.vue
+++ pages/aplicaciones.vue
@@ -1,8 +1,41 @@
 <template>
   <div>
     <section data-aos="fade-up">
-      <ComingSoon />
+      <div class="subsection">
+        <div class="text-subsection-single">
+          <h2>Marca ciudad</h2>
+          <div class="small-container">
+            <p class="small">
+              Para evaluar el funcionamiento de SGrafoType en el ámbito de la creación de una marca ciudad, se desarrollaron varias aplicaciones, construyendo gradualmente un sistema y estableciendo directrices que fueron recopiladas en un manual de marca. Este manual abarca la estrategia de marca, la identidad verbal y la identidad visual.
+            </p>
+            <p class="small">
+              Se diseñó un nuevo logotipo, concebido para ser dinámico, que integra SGrafoType en su construcción. Además, se definió una nueva paleta de colores, se seleccionó una tipografía para los textos, se estableció un estilo de iconos y se determinó un enfoque fotográfico particular.
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
+
+    
+
+
+    <img src="/public/assets/valla.png" data-aos="fade-up" />
+    <img src="/public/assets/posters.png" data-aos="fade-up" />
+    <img src="/public/assets/revistas.png" data-aos="fade-up" />
+    <img src="/public/assets/ppt.png" data-aos="fade-up" />
+    <ClientOnly>
+      <video autoplay muted loop data-aos="fade-up">
+        <source src="/public/assets/posts.mp4" type="video/mp4" />
+      </video>
+    </ClientOnly>
+
+    <ClientOnly>
+      <video autoplay muted loop data-aos="fade-up">
+        <source src="/public/assets/stories.mp4" type="video/mp4" />
+      </video>
+    </ClientOnly>
+    <img src="/public/assets/bus.png" data-aos="fade-up" />
+    <img src="/public/assets/equipo.png" data-aos="fade-up" />
   </div>
 </template>
 
--- pages/inicio.vue
+++ pages/inicio.vue
@@ -8,11 +8,14 @@
             visual de Segovia, equilibrando la atracción turística con la
             conexión local. Busca fortalecer el vínculo entre Segovia y sus
             habitantes, proyectando al mismo tiempo una imagen atractiva y
-            auténtica al exterior. SGrafoType, una familia tipográfica creada
-            específicamente para el proyecto, es el corazón de un proyecto que
-            busca dotar a Segovia de una identidad gráfica única. Un enfoque que
-            respeta su legado, refleja su espíritu acogedor y permite comunicar
-            su esencia de manera coherente y auténtica al mundo.
+            auténtica al exterior.
+          </p>
+          <p class="big">
+            SGrafoType, una familia tipográfica creada específicamente para el
+            proyecto, es el corazón de un proyecto que busca dotar a Segovia de
+            una identidad gráfica única. Un enfoque que respeta su legado,
+            refleja su espíritu acogedor y permite comunicar su esencia de
+            manera coherente y auténtica al mundo.
           </p>
         </div>
       </div>
@@ -25,15 +28,14 @@
             <p class="small">
               El turismo es esencial para la economía segoviana, y por ello las
               marcas promocionales se han enfocado en atraer visitantes
-              externos, descuidando a la comunidad local. Estas identidades,
-              diseñadas desde las demandas externas y proyectadas hacia dentro,
-              han dado lugar a marcas efímeras que no reflejan la esencia ni la
-              personalidad auténtica de la ciudad. Desde el diseño gráfico, esto
-              refleja una desconexión con la población local y sus
-              problemáticas, lo que supone un riesgo para el futuro de Segovia.
-              Este enfoque en el turismo y la población flotante ha generado en
-              los segovianos la percepción de que se está descuidando el
-              carácter local de la ciudad.
+              externos. Estas marcas, diseñadas desde las demandas externas y
+              proyectadas hacia dentro, han dado lugar a identidades efímeras
+              que no reflejan la esencia ni la personalidad auténtica de la
+              ciudad. Desde el diseño gráfico, esto evidencia una desconexión
+              con la población local y sus problemáticas, que suponen un riesgo
+              para el futuro de Segovia. El enfoque en el turismo y la población
+              flotante ha generado en los segovianos la percepción de que se
+              está descuidando el carácter local de la ciudad.
             </p>
           </div>
         </div>
@@ -82,6 +84,16 @@
               amor, el hogar y el sentido de comunidad de una ciudad que, por su
               reducido tamaño, acoge y conecta a quienes la habitan o visitan.
             </p>
+            <p class="small">
+              Con esta narrativa, ponemos el punto de partida en el carácter de
+              la ciudad para descubrir su auténtica identidad, al mismo tiempo
+              que promovemos la integración y conciliación entre la población
+              flotante y la local. Queremos comunicar que todos los agentes
+              contribuyen a la construcción de la identidad de Segovia. Además,
+              se presenta a la ciudad como un lugar en constante movimiento,
+              donde las actividades y eventos la mantienen en una continua
+              transformación.
+            </p>
           </div>
         </div>
       </div>
@@ -107,7 +119,19 @@
         </div>
         <div class="enumeracion">
           <div class="enumeracion-item">
-            <p class="numeral">01</p>
+            <!-- <p class="numeral">01</p> -->
+            <svg
+              class="numeral-svg"
+              viewBox="0 0 318 258"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M97.92 257.04C69.12 257.04 45.6 245.1 27.36 221.22C9.12 197.34 0 166.44 0 128.52C0 90.6 9.12 59.7 27.36 35.82C45.6 11.94 69.12 0 97.92 0C126.72 0 150.6 11.94 168.84 35.82C187.08 59.7 196.2 90.6 196.2 128.52C196.2 166.44 187.08 197.34 168.84 221.22C150.6 245.1 126.96 257.04 97.92 257.04ZM97.92 203.4C110.16 203.4 120.06 196.44 127.62 182.52C135.18 168.6 138.96 150.6 138.96 128.52C138.96 106.44 135.18 88.44 127.62 74.52C120.06 60.6 110.16 53.64 97.92 53.64C85.68 53.64 76.14 60.66 68.58 74.7C61.02 88.74 57.24 106.68 57.24 128.52C57.24 150.36 61.02 168.3 68.58 182.34C76.14 196.38 85.92 203.4 97.92 203.4ZM277.88 4.32H317.48V252.72H261.68V75.6C248.24 86.4 232.4 91.8 214.16 91.8V39.24C226.88 39.24 238.82 36.18 249.98 30.06C261.14 23.94 270.44 15.36 277.88 4.32Z"
+                fill="#002F6C"
+              />
+            </svg>
+
             <p class="numeral-text small">
               Es fruto de la herencia multicultural de Segovia, y por tanto es
               ejemplo de la riqueza cultural derivada del flujo constante de
@@ -116,7 +140,19 @@
           </div>
           <div class="enumeracion-blank"></div>
           <div class="enumeracion-item">
-            <p class="numeral">02</p>
+            <!-- <p class="numeral">02</p> -->
+            <svg
+              class="numeral-svg"
+              viewBox="0 0 389 258"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M97.92 257.04C69.12 257.04 45.6 245.1 27.36 221.22C9.12 197.34 0 166.44 0 128.52C0 90.6 9.12 59.7 27.36 35.82C45.6 11.94 69.12 0 97.92 0C126.72 0 150.6 11.94 168.84 35.82C187.08 59.7 196.2 90.6 196.2 128.52C196.2 166.44 187.08 197.34 168.84 221.22C150.6 245.1 126.96 257.04 97.92 257.04ZM97.92 203.4C110.16 203.4 120.06 196.44 127.62 182.52C135.18 168.6 138.96 150.6 138.96 128.52C138.96 106.44 135.18 88.44 127.62 74.52C120.06 60.6 110.16 53.64 97.92 53.64C85.68 53.64 76.14 60.66 68.58 74.7C61.02 88.74 57.24 106.68 57.24 128.52C57.24 150.36 61.02 168.3 68.58 182.34C76.14 196.38 85.92 203.4 97.92 203.4ZM281.05 200.16H388.33V252.72H216.97V217.8C216.97 207.48 219.01 197.82 223.09 188.82C227.17 179.82 232.51 171.96 239.11 165.24C245.71 158.52 253.03 152.22 261.07 146.34C269.11 140.46 277.15 134.88 285.19 129.6C293.23 124.32 300.55 119.22 307.15 114.3C313.75 109.38 319.09 104.04 323.17 98.28C327.25 92.52 329.29 86.64 329.29 80.64C329.29 73.2 326.77 67.2 321.73 62.64C316.69 58.08 310.21 55.8 302.29 55.8C294.37 55.8 287.95 58.08 283.03 62.64C278.11 67.2 275.65 73.44 275.65 81.36H218.41C218.41 58.08 226.45 38.7 242.53 23.22C258.61 7.74 278.77 0 303.01 0C327.25 0 346.63 7.44 362.59 22.32C378.55 37.2 386.53 55.92 386.53 78.48C386.53 90.96 383.89 102.54 378.61 113.22C373.33 123.9 366.31 133.26 357.55 141.3C348.79 149.34 339.97 156.48 331.09 162.72C322.21 168.96 312.85 175.5 303.01 182.34C293.17 189.18 285.85 195.12 281.05 200.16Z"
+                fill="#002F6C"
+              />
+            </svg>
+
             <p class="numeral-text small">
               Su versatilidad y atractivo visual lo hacen ideal para crear una
               identidad dinámica y duradera.
@@ -124,7 +160,19 @@
           </div>
           <div class="enumeracion-blank"></div>
           <div class="enumeracion-item">
-            <p class="numeral">03</p>
+            <!-- <p class="numeral">03</p> -->
+            <svg
+              class="numeral-svg"
+              viewBox="0 0 393 258"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M97.92 257.04C69.12 257.04 45.6 245.1 27.36 221.22C9.12 197.34 0 166.44 0 128.52C0 90.6 9.12 59.7 27.36 35.82C45.6 11.94 69.12 0 97.92 0C126.72 0 150.6 11.94 168.84 35.82C187.08 59.7 196.2 90.6 196.2 128.52C196.2 166.44 187.08 197.34 168.84 221.22C150.6 245.1 126.96 257.04 97.92 257.04ZM97.92 203.4C110.16 203.4 120.06 196.44 127.62 182.52C135.18 168.6 138.96 150.6 138.96 128.52C138.96 106.44 135.18 88.44 127.62 74.52C120.06 60.6 110.16 53.64 97.92 53.64C85.68 53.64 76.14 60.66 68.58 74.7C61.02 88.74 57.24 106.68 57.24 128.52C57.24 150.36 61.02 168.3 68.58 182.34C76.14 196.38 85.92 203.4 97.92 203.4ZM356.4 123.48C367.44 129.72 376.14 138.12 382.5 148.68C388.86 159.24 392.04 170.64 392.04 182.88C392.04 203.52 383.4 221.04 366.12 235.44C348.84 249.84 327.96 257.04 303.48 257.04C279 257.04 257.1 249.36 239.94 234C222.78 218.64 214.2 199.68 214.2 177.12H271.44C271.44 184.56 274.56 190.8 280.8 195.84C287.04 200.88 294.48 203.4 303.12 203.4C311.76 203.4 318.9 200.88 325.26 195.84C331.62 190.8 334.8 184.2 334.8 176.04C334.8 167.88 331.5 160.98 324.9 155.34C318.3 149.7 309.84 146.88 299.52 146.88H269.28V101.88H299.52C308.4 101.88 315.78 99.54 321.66 94.86C327.54 90.18 330.48 84.24 330.48 77.04C330.48 69.84 327.78 64.14 322.38 59.94C316.98 55.74 310.68 53.64 303.48 53.64C295.8 53.64 289.26 55.8 283.86 60.12C278.46 64.44 275.76 69.84 275.76 76.32H218.52C218.52 54.72 226.68 36.6 243 21.96C259.32 7.32 279.48 0 303.48 0C327.48 0 346.62 6.78 363.06 20.34C379.5 33.9 387.72 50.52 387.72 70.2C387.72 81.24 384.9 91.38 379.26 100.62C373.62 109.86 366 117.48 356.4 123.48Z"
+                fill="#002F6C"
+              />
+            </svg>
+
             <p class="numeral-text small">
               Es un recurso único y valioso, menos conocido que otros íconos de
               Segovia, y merece mayor visibilidad.
@@ -168,13 +216,25 @@
 
 <script setup>
 definePageMeta({
-  title: 'Inicio | SGrafoType',
+  title: "Inicio | SGrafoType",
   meta: [
-    { name: 'description', content: 'SGrafoType, una familia tipográfica creada específicamente para el proyecto, es el corazón de un proyecto que busca dotar a Segovia de una identidad gráfica única. Un enfoque que respeta su legado, refleja su espíritu acogedor y permite comunicar su esencia de manera coherente y auténtica al mundo.' },
-    { property: 'og:title', content: 'Inicio | SGrafoType' },
-    { property: 'og:description', content: 'SGrafoType, una familia tipográfica creada específicamente para el proyecto, es el corazón de un proyecto que busca dotar a Segovia de una identidad gráfica única. Un enfoque que respeta su legado, refleja su espíritu acogedor y permite comunicar su esencia de manera coherente y auténtica al mundo.' },
-    { name: 'twitter:title', content: 'Inicio | SGrafoType' },
-    { name: 'twitter:description', content: 'SGrafoType, una familia tipográfica creada específicamente para el proyecto, es el corazón de un proyecto que busca dotar a Segovia de una identidad gráfica única. Un enfoque que respeta su legado, refleja su espíritu acogedor y permite comunicar su esencia de manera coherente y auténtica al mundo.' }
-  ]
+    {
+      name: "description",
+      content:
+        "SGrafoType, una familia tipográfica creada específicamente para el proyecto, es el corazón de un proyecto que busca dotar a Segovia de una identidad gráfica única. Un enfoque que respeta su legado, refleja su espíritu acogedor y permite comunicar su esencia de manera coherente y auténtica al mundo.",
+    },
+    { property: "og:title", content: "Inicio | SGrafoType" },
+    {
+      property: "og:description",
+      content:
+        "SGrafoType, una familia tipográfica creada específicamente para el proyecto, es el corazón de un proyecto que busca dotar a Segovia de una identidad gráfica única. Un enfoque que respeta su legado, refleja su espíritu acogedor y permite comunicar su esencia de manera coherente y auténtica al mundo.",
+    },
+    { name: "twitter:title", content: "Inicio | SGrafoType" },
+    {
+      name: "twitter:description",
+      content:
+        "SGrafoType, una familia tipográfica creada específicamente para el proyecto, es el corazón de un proyecto que busca dotar a Segovia de una identidad gráfica única. Un enfoque que respeta su legado, refleja su espíritu acogedor y permite comunicar su esencia de manera coherente y auténtica al mundo.",
+    },
+  ],
 });
 </script>
